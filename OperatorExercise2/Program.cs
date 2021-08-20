85e7aaf4dced9918511bb6e43220fd2caae7d1f5
Amoriss <amoriss@protonmail.com>
2021-08-20 15:18:45 -0500

    initial commit

---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorExercise2
+{
+    class Program
+
+    {
+        public static void areaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            areaOfCircle();
+
+        }
+    } 
+}
+
+
+     
